0d2027d8c4d31affc50a2f88f3759edcd3f96b49
andrreew <47315993+andrreew@users.noreply.github.com>
2021-04-14 14:24:05 +0300

    Update SberBank.php

--- SberBank.php
+++ SberBank.php
@@ -22,14 +22,14 @@ $data = urldecode($postData);
 
 parse_str($data, $params);
 
-$deposit = $params['deposit'];
+$deposit = $params['deposit'] * 100;
 $rcode   = $params['rcode'];
 $ok_url  = $params['ok_url'];
 $ko_url  = $params['ko_url'];
 $email   = $params['email'];
 
 
-$ch = curl_init($pay_url . "payment/rest/register.do?amount=" . intval($deposit) . "00&currency=643&language=ru&orderNumber='" . $rcode . "'&password=" . $password . "&userName=" . $userName . "&returnUrl=" . $ok_url . "&failUrl=" . $ko_url . "&email=" . $email);
+$ch = curl_init($pay_url . "payment/rest/register.do?amount=" . intval($deposit) . "&currency=643&language=ru&orderNumber='" . $rcode . "'&password=" . $password . "&userName=" . $userName . "&returnUrl=" . $ok_url . "&failUrl=" . $ko_url . "&email=" . $email);
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
